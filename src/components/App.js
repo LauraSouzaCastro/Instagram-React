8e8f48c045211ce769358adf5069eed0fb261ad3
Laura <laurasouzacastro1@gmail.com>
2022-11-20 01:48:28 -0300

    componente app criado

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// import NavBar from "./NavBar";
+// import Corpo from "./Corpo";
+// import BarraMobile from "./BarraMobile";
+export default function App(){
+    return (
+        <>
+            {/* <NavBar />
+            <Corpo />
+            <BarraMobile /> */}
+        </>
+    )
+}
